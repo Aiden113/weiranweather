8e67a3c014354bab264ad2d195d22937c3ec6466
kylinSong <songkylin@163.com>
2017-10-24 19:17:18 +0800

    新增切换城市和手动更新天气的功能。

--- app/src/main/java/org/kylin/weiranweather/WeatherActivity.java
+++ app/src/main/java/org/kylin/weiranweather/WeatherActivity.java
@@ -4,11 +4,15 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.preference.PreferenceManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -29,6 +33,8 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class WeatherActivity extends AppCompatActivity {
+    public DrawerLayout drawerLayout;
+    private Button navButton;
     private static final String TAG = "WeatherActivity";
     private ScrollView weatherLayout;
     private TextView titleCity;
@@ -43,6 +49,8 @@ public class WeatherActivity extends AppCompatActivity {
     private TextView sportText;
 private ImageView bgImg;
 
+    public SwipeRefreshLayout swipeRefresh;
+    private String mWeatherId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,6 +72,18 @@ private ImageView bgImg;
         comforText = (TextView) findViewById(R.id.comfort_text);
         carWashText = (TextView) findViewById(R.id.car_wash_text);
         sportText = (TextView) findViewById(R.id.sport_text);
+        swipeRefresh= (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
+
+        drawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
+        navButton= (Button) findViewById(R.id.nav_button);
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
         SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString =prefs.getString("weather",null);
         bgImg= (ImageView) findViewById(R.id.bg_img);
@@ -76,23 +96,20 @@ private ImageView bgImg;
         if (weatherString!=null){
             //有缓存时直接解析天气数据
             Weather weather = Utility.handleWeatherResponse(weatherString);
-            Log.d(TAG, "weather: weather.toString();"+weather.forecastlist.get(0).getDate());
-//            Log.d(TAG, "weather: getTxt;"+weather.suggestion.getSuggestion().getComf().getTxt());
-//            String comfort="舒适度："+weather.suggestion.getSuggestion().getComf().getTxt();
-
-            Log.d(TAG, "weather: getTxt;"+weather.suggestion.comf.brf);
-            Log.d(TAG, "weather: getTxt;"+weather.suggestion.comf.txt);
-            String comfort="舒适度："+weather.suggestion.cw.txt;
-            Log.d(TAG, "weather: comfort ;"+comfort);
-//            String carWash="洗车指数："+weather.suggestion.getSuggestion().getCw().getTxt();
-
+            mWeatherId=weather.basic.weatherId;
             showWeatherInfo(weather);
         }else {
             //无缓存时去服务器查询天气
-            String weatherId =getIntent().getStringExtra("weather_id");
+            mWeatherId  =getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);
-            requestWeather(weatherId);
+            requestWeather(mWeatherId);
         }
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                requestWeather(mWeatherId);
+            }
+        });
     }
 
     /**
@@ -122,7 +139,7 @@ private ImageView bgImg;
         });
     }
 
-    private void requestWeather(final String weatherId) {
+    public void requestWeather(final String weatherId) {
         Log.d(TAG, "requestWeather:weatherId: "+weatherId);
         String weatherUrl = "http://guolin.tech/api/weather?cityid="+weatherId+"&key=ee80d46ea2034d1c9146e36e67a3d074";
         HttpUtil.sendOkhttpRequest(weatherUrl, new Callback() {
@@ -144,6 +161,7 @@ private ImageView bgImg;
                         }else{
                             Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
                         }
+                        swipeRefresh.setRefreshing(false);
                     }
                 });
             }
@@ -154,6 +172,7 @@ private ImageView bgImg;
                     @Override
                     public void run() {
                         Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
+                        swipeRefresh.setRefreshing(false);
                     }
                 });
             }
@@ -199,7 +218,7 @@ private ImageView bgImg;
         }
         String comfort="舒适度："+weather.suggestion.comf.txt;
         String carWash="洗车指数："+weather.suggestion.cw.txt;
-        String sport="洗车指数："+weather.suggestion.sport.txt;
+        String sport="运动建议："+weather.suggestion.sport.txt;
         comforText.setText(comfort);
         carWashText.setText(carWash);
         sportText.setText(sport);
